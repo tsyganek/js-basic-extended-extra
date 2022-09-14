e2b5f78cbbbd4fe3f9f14147eca82d676e3ffd64
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-09-14 15:53:54 +0300

    extra2 solved

--- js/extra2.js
+++ js/extra2.js
@@ -1,5 +1,21 @@
 // Напишіть функцію findSmallerDigits(arr), яка приймає масив чисел arr та повертає новий масив чисел resultArr. Кожне число нового масиву resultArr[i] - це кількість чисел праворуч від arr[i], які менші за arr[i].
-function findSmallerDigits(arr) {}
+function findSmallerDigits(arr) {
+  let newArr = [];
+
+  arr.forEach(function (item, i, arrr) {
+    let arr2 = arrr.slice(i + 1, arrr.length);
+
+    let result = [];
+    for (let j = 0; j < arr2.length; j++) {
+      if (item > arr2[j]) {
+        result.push(arr2[j]);
+      }
+    }
+    newArr.push(result.length);
+  });
+  console.log(newArr);
+  return newArr;
+}
 
 findSmallerDigits([5, 4, 3, 2, 1]);
 // === [4, 3, 2, 1, 0]
